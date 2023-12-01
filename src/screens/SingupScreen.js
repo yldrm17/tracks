029eef128437dd890883315db6fbc7488b97a4e5
enesyldrm <enes082317@gmail.com>
2023-12-02 01:45:46 +0300

    SingupScreen

--- src/screens/SingupScreen.js
+++ src/screens/SingupScreen.js
@@ -1,14 +1,59 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+import { color } from "react-native-elements/dist/helpers";
 
-const SingupScreen = () => {
+const SingupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SingupScreen</Text>
+    <View style={styles.container}>
+      <Spacer>
+        <Text style={{ textAlign: "center" }} h3>
+          Sing Up for Tracker
+        </Text>
+      </Spacer>
+      <Input
+        value={email}
+        onChangeText={setEmail}
+        label="Email"
+        placeholder="email@address.com"
+        labelStyle={{ fontWeight: "bold", color: "black" }}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+
+      <Input
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+        label="Password"
+        placeholder="Enter the password"
+        labelStyle={{ fontWeight: "bold", color: "black" }}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer>
+        <Button title="Sing Up" />
+      </Spacer>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SingupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
 
 export default SingupScreen;
